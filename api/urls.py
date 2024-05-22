d36494fb81ca24daf4385665700dc58589959699
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-05-22 21:41:42 +0000

    feat:update routes

--- api/urls.py
+++ api/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('devblog_api.urls')),
+    path('api/token/', include('auth_api.urls')),
+    path('api/', include('devblog_api.urls')),
 ]
