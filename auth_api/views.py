1b2c81d8bc81e0655c545b7cc9df848116e7e7ab
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-05-23 03:43:14 +0000

    feat:user create view

--- auth_api/views.py
+++ auth_api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from .serializers import UserCreateSerializer
 
 # Create your views here.
+
+class UserCreateView(CreateAPIView):
+    serializer_class = UserCreateSerializer
+    permission_classes = [AllowAny]
