11320f7df787d055438fa2c8ad421e97b36be1be
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-07-15 23:07:46 +0000

    add image and post

--- posts_api/models.py
+++ posts_api/models.py
@@ -1,8 +1,12 @@
+from typing import Iterable
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    slug = models.SlugField(blank=True)
+    image = models.URLField(blank=False, null=False)
     description = models.CharField(max_length=256)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
@@ -10,3 +14,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.author.username + ': ' + self.title
+    
+    def save(self, *args,**kwargs):
+        self.slug = slugify(self.title+str(self.pk))
+        super().save(*args, **kwargs)
--- posts_api/serializers.py
+++ posts_api/serializers.py
@@ -2,7 +2,8 @@ from .models import Post
 from rest_framework import serializers
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.StringRelatedField()
     class Meta:
         model = Post
         fields = '__all__'
+        read_only_fields = ['author','slug','date_posted']
