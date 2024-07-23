45f767a43f121868fe5ff743805e8db76fe146e8
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-07-23 01:13:21 +0000

    apply author filter

--- authors_api/views.py
+++ authors_api/views.py
@@ -1,6 +1,8 @@
 from posts_api.models import Post
 from posts_api.serializers import PostSerializer
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -8,5 +10,16 @@ class AuthorPostListView(ListAPIView):
     """
     API endpoint that list author post.
     """
-    queryset = Post.objects.all()
+
     serializer_class = PostSerializer
+
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        user_id = self.request.user.token['user_id']
+        author = User.objects.get(id=user_id)
+
+        queryset = Post.objects.filter(author=author)
+        serializer_class = self.get_serializer_class()
+        
+        serializer = serializer_class(queryset, many=True)
+        return Response(serializer.data)
